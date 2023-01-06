b1e78c5dfe8f7cd2b34991f400beb8dc1c96cf73
Vic Godoy <61629307+mvigodoy@users.noreply.github.com>
2023-01-06 12:04:02 -0300

    mock de tareas e interfaces de las mismas

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar modulo de Angular",
+        day: '5 de diciembre a las 12'
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar modulo de Angular",
+        day: '5 de diciembre a las 12'
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Terminar modulo de Angular",
+        day: '5 de diciembre a las 12'
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Terminar modulo de Angular",
+        day: '5 de diciembre a las 12'
+        reminder: true
+    }
+]
